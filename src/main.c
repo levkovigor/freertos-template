4b541e33fe2e35fd1fa4c3c2413a083d8eabda12
Colin González <colin.gonzalez@apc.in2p3.fr>
2018-09-19 14:59:23 +0200

    Demo with isis lib

--- src/main.c
+++ src/main.c
@@ -6,11 +6,7 @@
 #include <pit/pit.h>
 #include <tc/tc.h>
 #include <aic/aic.h>
-#include <utility/led.h>
 #include <utility/trace.h>
-#include <twi/twi.h>
-#include <drivers/async/async.h>
-#include <drivers/twi/twid.h>
 #include <stdio.h>
 
 #include <freertos/FreeRTOS.h>
@@ -22,39 +18,44 @@
 #include <hal/boolean.h>
 #include <hal/version/version.h>
 
-#include <hal/Drivers/UART.h>
+#include <hal/Drivers/LED.h>
 #include <hal/interruptPriorities.h>
 #include <hal/boolean.h>
 #include <hal/Utility/util.h>
-#include "I2Ctest.h"
+#include <hal/Timing/WatchDogTimer.h>
+#include "checksumTest.h"
 
 #define ENABLE_MAIN_TRACES 1
 #if ENABLE_MAIN_TRACES
-	#define MAIN_TRACE_INFO			TRACE_INFO
-	#define MAIN_TRACE_DEBUG		TRACE_DEBUG
-	#define MAIN_TRACE_WARNING		TRACE_WARNING
-	#define MAIN_TRACE_ERROR		TRACE_ERROR
-	#define MAIN_TRACE_FATAL		TRACE_FATAL
+#define MAIN_TRACE_INFO			TRACE_INFO
+#define MAIN_TRACE_DEBUG		TRACE_DEBUG
+#define MAIN_TRACE_WARNING		TRACE_WARNING
+#define MAIN_TRACE_ERROR		TRACE_ERROR
+#define MAIN_TRACE_FATAL		TRACE_FATAL
 #else
-	#define MAIN_TRACE_INFO(...)	{ }
-	#define MAIN_TRACE_DEBUG(...)	{ }
-	#define MAIN_TRACE_WARNING(...)	{ }
-	#define MAIN_TRACE_ERROR		TRACE_ERROR
-	#define MAIN_TRACE_FATAL		TRACE_FATAL
+#define MAIN_TRACE_INFO(...)	{ }
+#define MAIN_TRACE_DEBUG(...)	{ }
+#define MAIN_TRACE_WARNING(...)	{ }
+#define MAIN_TRACE_ERROR		TRACE_ERROR
+#define MAIN_TRACE_FATAL		TRACE_FATAL
 #endif
 
-const Pin twi_pins = PINS_TWI;
 
-Twid twid;
+const signed char * const task_name_led_blink =
+  (const signed char * const ) "led_blink";
 
-void led_blink(void *pParams){
-//WDT_startWatchdogKickTask(10 / portTICK_RATE_MS, FALSE);
-  I2Ctest();
+void *task_led_blink(){
+  WDT_startWatchdogKickTask(10 / portTICK_RATE_MS, FALSE);
+  checksumTest();
+  LED_start();
+  LED_glow(led_1);
   do{
-    LED_Toggle(1);
+    printf("Test\n\r");
+    LED_toggle(led_1);
     vTaskDelay(2000);
   }
   while(1);
+  return NULL;
 }
 
 
@@ -62,25 +63,15 @@ int main(void)
 {
   // DBGU output configuration
   TRACE_CONFIGURE_ISP(DBGU_STANDARD, 115200, BOARD_MCK);
-  CP15_EnableIcache();
+  CP15_Enable_I_Cache();
 
-  printf("-- Getting Started Project %s --\n\r", SOFTPACK_VERSION);
+  printf("-- ISIS Template Project %s --\n\r", SOFTPACK_VERSION);
   printf("-- %s\n\r", BOARD_NAME);
   printf("-- Compiled: %s %s --\n\r", __DATE__, __TIME__);
 
+  WDT_start();
   
-  PMC_EnablePeripheral(AT91C_ID_TWI);
-  PIO_Configure(&twi_pins,1);
-  TWI_ConfigureMaster(AT91C_BASE_TWI,100000,BOARD_MCK);
-  twid.pTwi = AT91C_BASE_TWI;
-  twid.pTwi = NULL;
-  TWID_Initialize(&twid, AT91C_BASE_TWI);
-  TWID_Write(&twid, 0x22, 0x0, 0x0, (unsigned char *) "hello\n\r",7,NULL);
-
-  
-  LED_Configure(1);
-
-  xTaskCreate(led_blink, (const signed char*) "led_blink", configMINIMAL_STACK_SIZE,
+  xTaskCreate(task_led_blink, task_name_led_blink, configMINIMAL_STACK_SIZE,
 	      NULL, configMAX_PRIORITIES - 1, NULL );
   vTaskStartScheduler();
 }
